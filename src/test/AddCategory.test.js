8f396e408368ca2507ac36c8371e1448bd026b3a
Jesus Alejandro Garcia <garciacode96@gmail.com>
2021-09-08 23:14:06 -0500

    Tests con mocks

--- src/test/AddCategory.test.js
+++ src/test/AddCategory.test.js
@@ -5,11 +5,6 @@ import '@testing-library/jest-dom';
 import AddCategory from '../components/AddCategory/AddCategory';
 
 describe('Pruebas unitarias para componente AddCategory', () => {
-  const agregarCategoria = () => [
-    'One Punch Man',
-    'Dragon Ball Super',
-    'Pokemon',
-  ];
   let wrapper;
   const setCategorias = jest.fn();
 
@@ -18,7 +13,7 @@ describe('Pruebas unitarias para componente AddCategory', () => {
     // pueda existir (valor por defecto para hacer test) y reiniciamos
     // el componente antes de ejecutar las pruebas
     jest.clearAllMocks();
-    wrapper = shallow(<AddCategory addCategory={agregarCategoria} />);
+    wrapper = shallow(<AddCategory addCategory={setCategorias} />);
   });
 
   test('Render del componente', () => {
@@ -45,5 +40,25 @@ describe('Pruebas unitarias para componente AddCategory', () => {
       preventDefault() {},
     });
     expect(setCategorias).not.toHaveBeenCalled();
+  });
+
+  // No se guarda el wrapper en una constante o variable porque solo almacena
+  // el valor inicial del test, después aun mutando el wrapper al parecer no
+  // se modifica y son distintos y no pasa el test (investigar sobre eso)
+  test('Se debe llamar el setCategorias y se debe limpiar el input', () => {
+    const valorInput = 'Dragon Ball Super';
+
+    wrapper.find('input').simulate('change', {
+      target: {
+        value: valorInput,
+      },
+    });
+
+    wrapper.find('form').simulate('submit', {
+      preventDefault() {},
+    });
+
+    expect(setCategorias).toHaveBeenCalled();
+    expect(wrapper.find('input').prop('value')).toBe('');
   });
 });
--- src/test/GifGrid.test.js
+++ src/test/GifGrid.test.js
@@ -1,29 +1,78 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import '@testing-library/jest-dom';
 
 import GifGrid from '../components/GifGrid/GifGrid';
+import { useFetchGifs } from '../hooks/useFetchGifs';
 
+jest.mock('../hooks/useFetchGifs');
+
+// Se usa el mock en todos los test antes de usar el shallow
+// de otra manera habra un error en la desestructuración de GifGrid
 describe('Testing de componentes con Enzyme', () => {
   const categoria = 'One Punch Man';
   const claseContenedor = 'animate__slideInUp';
-  const wrapper = shallow(<GifGrid category={categoria} />);
 
   test('Debe mostrar (renderizar) <GifGrid /> de manera correcta', () => {
-    expect(wrapper).toMatchSnapshot();
+    useFetchGifs.mockReturnValue({
+      data: [],
+      loading: true,
+    });
+    const componente = shallow(<GifGrid category={categoria} />);
+    expect(componente).toMatchSnapshot();
   });
 
   test('La categoria debe ser una cadena valida', () => {
-    const titulo = wrapper.find('.gif-grid__title');
-    expect(titulo.text().trim()).toBe(categoria);
+    useFetchGifs.mockReturnValue({
+      data: [],
+      loading: true,
+    });
+    const wrapper = shallow(<GifGrid category={categoria} />);
+    expect(wrapper.find('.gif-grid__title').text().trim()).toBe(categoria);
   });
 
   test('Verificar que la clase sea correcta', () => {
-    const contenedorComponente = wrapper.find('.gif-grid').prop('className');
-    expect(contenedorComponente.split(' ')).toContain(claseContenedor);
+    useFetchGifs.mockReturnValue({
+      data: [],
+      loading: true,
+    });
+    const wrapper = shallow(<GifGrid category={categoria} />);
+    expect(wrapper.find('.gif-grid').prop('className').split(' ')).toContain(
+      claseContenedor
+    );
   });
 
   test('Validar que la url sea de una imagen', () => {
-    const urlImagenRenderizada = wrapper.find('img').at(0);
-    console.log(urlImagenRenderizada);
+    const fakeDataResponse = [
+      {
+        id: 1,
+        title: 'Saitama',
+        urlGif: 'https://localhost/some/saitama.png',
+      },
+    ];
+    useFetchGifs.mockReturnValue({
+      data: fakeDataResponse,
+      loading: false,
+    });
+    const wrapper = shallow(<GifGrid category={categoria} />);
+    console.log(wrapper.find('img').at(0));
+  });
+
+  // Llama el hook de fetch y usa un mock para simular que si respondio
+  // y verificamos que coincida con el sanpshot
+  test('Debe mostrar items cuando se carga el componente', () => {
+    const fakeDataResponse = [
+      {
+        id: 1,
+        title: 'Saitama',
+        urlGif: 'https://localhost/some/saitama.png',
+      },
+    ];
+    useFetchGifs.mockReturnValue({
+      data: fakeDataResponse,
+      loading: false,
+    });
+    const componente = shallow(<GifGrid category={categoria} />);
+    expect(componente).toMatchSnapshot();
   });
 });
